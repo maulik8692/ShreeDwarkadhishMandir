fbbca7f9c84592b130ed5205a2976687b4f80c78
Maulik Shah <shah_maulik@live.com>
2021-09-23 19:37:17 +0530

    Samagri Transaction - page create and cache cleaner to .js

---
+++ ShreeDwarkadhishMandir/Controllers/SamagriTransactionController.cs
@@ -0,0 +1,141 @@
+﻿using CommonLayer;
+using ShreeDwarkadhishMandir.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShreeDwarkadhishMandir.Controllers
+{
+    public class SamagriTransactionController : Controller
+    {
+        // GET: SamagriTransaction
+        public ActionResult SamagriTransaction()
+        {
+            if (Function.ReadCookie(CookiesKey.AuthenticatedId).ToInt() == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
+            return View();
+        }
+
+        public ActionResult Issue()
+        {
+            if (Function.ReadCookie(CookiesKey.AuthenticatedId).ToInt() == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Issue(int id)
+        {
+            try
+            {
+                return Json("Samagri saved successfully.", JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Log.Write(ex);
+                return new HttpStatusCodeResult(410, ex.Message);
+            }
+        }
+
+        public ActionResult Scraped()
+        {
+            if (Function.ReadCookie(CookiesKey.AuthenticatedId).ToInt() == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Scraped(int id)
+        {
+            try
+            {
+                return Json("Samagri saved successfully.", JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Log.Write(ex);
+                return new HttpStatusCodeResult(410, ex.Message);
+            }
+        }
+
+        public ActionResult Purchase()
+        {
+            if (Function.ReadCookie(CookiesKey.AuthenticatedId).ToInt() == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Purchase(int id)
+        {
+            try
+            {
+                return Json("Samagri saved successfully.", JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Log.Write(ex);
+                return new HttpStatusCodeResult(410, ex.Message);
+            }
+        }
+
+        public ActionResult Donation()
+        {
+            if (Function.ReadCookie(CookiesKey.AuthenticatedId).ToInt() == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Donation(int id)
+        {
+            try
+            {
+                return Json("Samagri saved successfully.", JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Log.Write(ex);
+                return new HttpStatusCodeResult(410, ex.Message);
+            }
+        }
+
+        public ActionResult Complementary()
+        {
+            if (Function.ReadCookie(CookiesKey.AuthenticatedId).ToInt() == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Complementary(int id)
+        {
+            try
+            {
+                return Json("Samagri saved successfully.", JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                Log.Write(ex);
+                return new HttpStatusCodeResult(410, ex.Message);
+            }
+        }
+
+    }
+}
